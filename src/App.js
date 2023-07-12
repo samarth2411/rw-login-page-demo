4b3fa3b200bb5ca343d10e9eace092086c912462
Samarth Porwal <samarthkporwal@gmail.com>
2023-07-12 11:19:48 +0530

    added the light mode for desktop and mobile view
    added the light mode for desktop and mobile view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// $("#mobile_code").intlTelInput({
+//   initialCountry: "in",
+//   separateDialCode: true,
+// });
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Outer flex'>
+      <div className='illustration'>
+        <div className='image'>
+          <img className='im' src={require('./images/Mobile login-pana 2.png')} />
+        </div>
+      </div>
+      <div className='sign-up-side flex'>
+        <div className='content'>
+          <div className='first'>
+            <sapn className='head'>Sign up to improve your</sapn>
+            <span className='head'>Financial decisions</span>
+          </div>
+          <div className='second'>
+            <p className='mob'>Enter your Mobile Number</p>
+          </div>
+          <div className='third'>
+            <input type="text" id="mobile_code" class="form-control" placeholder="Phone Number" name="name" />
+          </div>
+          <div className='fourth'>
+            <span className='terms'>By proceeding, you agree with Ready Wealth’s terms </span>
+            <span className='terms'>and conditions and privacy policy</span>
+          </div>
+          <div className='fifth'>
+            <button className='btn-1' type="button" disabled><span className='btn'>Send OTP</span></button>
+          </div>
+          <div className='sixth flex'>
+            <span className='already-login'>Already have an account ? <span className='login'>Log In</span></span>
+          </div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
