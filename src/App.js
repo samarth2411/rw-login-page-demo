ca8a08df9e6bd8ab98d0ffa5602b3c5be258df99
Samarth Porwal <samarthkporwal@gmail.com>
2023-07-13 10:40:07 +0530

    added the dark theme
    added the dark theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
+import { createContext, useState } from 'react';
+import ReactSwitch from 'react-switch';
+
+
+export const ThemeContext = createContext(null);
 
 
 // $("#mobile_code").intlTelInput({
@@ -8,40 +12,49 @@ import './App.css';
 // });
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
   return (
-    <div className='Outer flex'>
-      <div className='illustration'>
-        <div className='image'>
-          <img className='im' src={require('./images/Mobile login-pana 2.png')} />
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className='Outer flex' id={theme}>
+        <div className='illustration'>
+          <div className='image'>
+            <img className='im' src={require('./images/Mobile login-pana 2.png')} />
+          </div>
+        </div>
+        <div className='sign-up-side flex'>
+          <div className='content'>
+            <div className='first'>
+              <span className='head'>Sign up to improve your</span>
+              <span className='head'>Financial decisions</span>
+            </div>
+            <div className='second'>
+              <p className='mob'>Enter your Mobile Number</p>
+            </div>
+            <div className='third'>
+              <input className="form-control" placeholder="Phone Number" />
+            </div>
+            <div className='fourth'>
+              <span className='terms'>By proceeding, you agree with Ready Wealth’s terms </span>
+              <span className='terms'>and conditions and privacy policy</span>
+            </div>
+            <div className='fifth'>
+              <button className='btn-1' type="button" disabled><span className='btn'>Send OTP</span></button>
+            </div>
+            <div className='sixth flex'>
+              <span className='already-login'>Already have an account ? <span className='login'>Log In</span></span>
+            </div>
+            <div className='seventh'>
+              <label>{theme === "light" ? "Light Mode " : "Dark Mode "}</label>
+              <ReactSwitch onChange={toggleTheme} checked={theme === "dark"}></ReactSwitch>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='sign-up-side flex'>
-        <div className='content'>
-          <div className='first'>
-            <sapn className='head'>Sign up to improve your</sapn>
-            <span className='head'>Financial decisions</span>
-          </div>
-          <div className='second'>
-            <p className='mob'>Enter your Mobile Number</p>
-          </div>
-          <div className='third'>
-            <input type="text" id="mobile_code" class="form-control" placeholder="Phone Number" name="name" />
-          </div>
-          <div className='fourth'>
-            <span className='terms'>By proceeding, you agree with Ready Wealth’s terms </span>
-            <span className='terms'>and conditions and privacy policy</span>
-          </div>
-          <div className='fifth'>
-            <button className='btn-1' type="button" disabled><span className='btn'>Send OTP</span></button>
-          </div>
-          <div className='sixth flex'>
-            <span className='already-login'>Already have an account ? <span className='login'>Log In</span></span>
-          </div>
-
-        </div>
-      </div>
-
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
